1d095eacf45ef0cfe1addf24dc9cc10d59cdcb4d
ryanbrwr <ryanbrew13@gmail.com>
2023-12-21 11:59:10 -0500

    Add: function for past

--- src/utils/dayjs.ts
+++ src/utils/dayjs.ts
@@ -1,9 +1,9 @@
-import dayjs from 'dayjs';
-import * as enLocale from 'dayjs/locale/en.js';
-import relativeTime from 'dayjs/plugin/relativeTime.js';
-import timezone from 'dayjs/plugin/timezone.js';
-import updateLocale from 'dayjs/plugin/updateLocale.js';
-import utc from 'dayjs/plugin/utc.js';
+import dayjs from "dayjs";
+import * as enLocale from "dayjs/locale/en.js";
+import relativeTime from "dayjs/plugin/relativeTime.js";
+import timezone from "dayjs/plugin/timezone.js";
+import updateLocale from "dayjs/plugin/updateLocale.js";
+import utc from "dayjs/plugin/utc.js";
 
 dayjs.extend(utc);
 dayjs.extend(timezone);
@@ -11,23 +11,23 @@ dayjs.extend(relativeTime);
 dayjs.extend(updateLocale);
 
 const locale = {
-	...enLocale,
-	relativeTime: {
-		// relative time format strings, keep %s %d as the same
-		future: 'in %s', // e.g. in 2 hours, %s been replaced with 2hours
-		past: '%s ago',
-		s: 'now',
-		m: '1m',
-		mm: '%dm',
-		h: '1h',
-		hh: '%dh', // e.g. 2 hours, %d been replaced with 2
-		d: '1d',
-		dd: '%dd',
-		M: '1mo',
-		MM: '%dmo',
-		y: '1yr',
-		yy: '%dyr'
-	}
+  ...enLocale,
+  relativeTime: {
+    // relative time format strings, keep %s %d as the same
+    future: "in %s", // e.g. in 2 hours, %s been replaced with 2hours
+    past: (v) => (v === "now" ? v : v + " ago"),
+    s: "now",
+    m: "1m",
+    mm: "%dm",
+    h: "1h",
+    hh: "%dh", // e.g. 2 hours, %d been replaced with 2
+    d: "1d",
+    dd: "%dd",
+    M: "1mo",
+    MM: "%dmo",
+    y: "1yr",
+    yy: "%dyr",
+  },
 };
 
 // load locale for later use
@@ -35,4 +35,4 @@ dayjs.locale(locale, undefined, true);
 
 export { locale };
 
-export { default as dayjs } from 'dayjs';
+export { default as dayjs } from "dayjs";
